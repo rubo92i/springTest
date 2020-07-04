c5bb26cd04ac3a4cfa0bf93464d475b5a2a80a81
rubo92i <ruben.manukyan@appish.co.uk>
2020-07-04 14:02:52 +0400

    t

--- src/main/java/am/basic/springTest/util/MailSenderClient.java
+++ src/main/java/am/basic/springTest/util/MailSenderClient.java
@@ -16,33 +16,6 @@ import javax.net.ssl.X509TrustManager;
 public class MailSenderClient {
 
 
-    @PostConstruct
-    public void init() {
-        TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return null;
-                    }
-
-                    public void checkClientTrusted(
-                            java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-
-                    public void checkServerTrusted(
-                            java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-                }
-        };
-
-         try {
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (Exception e) {
-        }
-
-    }
-
     @Autowired
     public JavaMailSender emailSender;
 
